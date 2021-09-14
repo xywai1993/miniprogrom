57660f2195be2025cf2e7dcc04d9ca10b99d5320
yiper <yiper.fan@gmail.com>
2021-09-14 08:45:59 +0800

    使用rollup打包npm模块

---
+++ build/rollup.js
@@ -0,0 +1,32 @@
+// const rollup = require('rollup');
+import { rollup } from 'rollup';
+import resolve from 'rollup-plugin-node-resolve';
+
+// see below for details on the options
+const inputOptions = {
+    input: './rollupTmp.js',
+    plugins: [resolve()],
+};
+
+const outputOptions = {
+    // dir: './dist',
+    file: 'bundle.js',
+    format: 'cjs',
+};
+
+export async function build() {
+    // create a bundle
+    const bundle = await rollup(inputOptions);
+
+    console.log(bundle.imports); // an array of external dependencies
+    console.log(bundle.exports); // an array of names exported by the entry point
+    console.log(bundle.modules); // an array of module objects
+
+    // generate code and a sourcemap
+    // const { code, map } = await bundle.generate(outputOptions);
+
+    // or write the bundle to disk
+    await bundle.write(outputOptions);
+}
+
+// build();
--- main.js
+++ main.js
@@ -2,16 +2,17 @@ import { visit, parse, run, print, types } from 'recast';
 import { parse as vueParse } from '@vue/compiler-sfc';
 import { readFileSync, writeFileSync } from 'fs';
 import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import { build as rollupBuild } from './build/rollup.js';
 
+const require = createRequire(import.meta.url);
 const code = readFileSync('./src/pages/main.vue', { encoding: 'utf-8' });
 
 const result = vueParse(code);
 
 const ast = parse(result.descriptor.script?.content || '');
-console.log(ast.program.body[0]);
-const b = types.builders;
 
+const b = types.builders;
+let promise = null;
 visit(ast, {
     visitImportDeclaration(path) {
         const node = path.node;
@@ -29,11 +30,23 @@ visit(ast, {
 
             // const createRollFile = b.program([node]);
             node.type = 'ExportNamedDeclaration';
+            // node.source.value = './bundle.js';
             // node.specifiers = node.specifiers.map((item) => (item.type = 'ExportSpecifier'));
-            writeFileSync('./test.js', print(node).code);
+            writeFileSync('./rollupTmp.js', print(node).code);
+            promise = rollupBuild()
+                .then(() => {
+                    node.source.value = './bundle.js';
+                    writeFileSync('./rollupTmp.js', print(node).code);
+                })
+                .catch(console.log);
         } catch (error) {
+            // 非npm模块
             // console.log(error, node.source.value);
         }
         return false;
     },
+});
+
+promise.then(() => {
+    console.log(print(ast).code);
 });
