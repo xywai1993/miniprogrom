c9d737d2e86be46b6011ca607c6d50013d96a815
yiper <yiper.fan@gmail.com>
2021-09-17 17:11:56 +0800

    准备发布npm

--- build/watch.js
+++ build/watch.js
@@ -1,24 +1,5 @@
 import watch from 'node-watch';
-import path from 'path';
+import path from 'path/posix';
 import { watchVueFile, watchJsFile, sourceDir, targetDir } from './main.js';
 import { copyFile } from 'fs/promises';
 import { usePathInfo } from './util.js';
-watch(sourceDir, { recursive: true }, function (evt, src) {
-    if (!src) {
-        return;
-    }
-    const { fileName, dirSrc, extName } = usePathInfo(src);
-    console.log(` changed success : ${src}  fileName:${fileName} extName: ${extName} `);
-    if (extName == '.vue') {
-        watchVueFile(src);
-    }
-    else if (extName == '.js') {
-        watchJsFile(src);
-    }
-    else {
-        const targetSrc = path.join(targetDir, path.relative(sourceDir, src));
-        copyFile(src, targetSrc).then(() => {
-            console.log(`copyFile success : ${src}--->${targetSrc}`);
-        });
-    }
-});
--- src/watch.ts
+++ src/watch.ts
@@ -1,26 +1,27 @@
-import { startTask } from '@yiper.fan/taskbuild';
 import watch from 'node-watch';
 import path from 'path';
 import { watchVueFile, watchJsFile, sourceDir, targetDir } from './main.js';
 import { copyFile } from 'fs/promises';
 import { usePathInfo } from './util.js';
 
-watch(sourceDir, { recursive: true }, function (evt, src) {
-    if (!src) {
-        return;
-    }
+export function watchSourceAndBuild(sourceDir: string, targetDir: string) {
+    watch(sourceDir, { recursive: true }, function (evt, src) {
+        if (!src) {
+            return;
+        }
+        const { fileName, dirSrc, extName } = usePathInfo(src);
+        console.log(` changed success : ${src}  fileName:${fileName} extName: ${extName} `);
+        if (extName == '.vue') {
+            watchVueFile(src);
+        } else if (extName == '.js') {
+            watchJsFile(src);
+        } else {
+            const targetSrc = path.join(targetDir, path.relative(sourceDir, src));
+            copyFile(src, targetSrc).then(() => {
+                console.log(`copyFile success : ${src}--->${targetSrc}`);
+            });
+        }
+    });
+}
 
-    const { fileName, dirSrc, extName } = usePathInfo(src);
-    console.log(` changed success : ${src}  fileName:${fileName} extName: ${extName} `);
-
-    if (extName == '.vue') {
-        watchVueFile(src);
-    } else if (extName == '.js') {
-        watchJsFile(src);
-    } else {
-        const targetSrc = path.join(targetDir, path.relative(sourceDir, src));
-        copyFile(src, targetSrc).then(() => {
-            console.log(`copyFile success : ${src}--->${targetSrc}`);
-        });
-    }
-});
+// watchSourceAndBuild(sourceDir, targetDir);
