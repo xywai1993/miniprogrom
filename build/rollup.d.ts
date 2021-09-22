803a0c606ad5c0a1227496c4301463f7a94fb4c6
yiper <yiper.fan@gmail.com>
2021-09-22 15:59:40 +0800

    test

--- build/rollup.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 打包
- * @param input filePath
- * @param key 打包后的文件名
- * @param targetDir 打包后的目标路径
- * @returns
- */
-export declare function build(input: string, key: string, targetDir: string): Promise<string>;
--- build/watch.d.ts
+++
@@ -1,6 +0,0 @@
-interface options {
-    sourceDir: string;
-    targetDir: string;
-}
-export declare function watchSourceAndBuild({ sourceDir, targetDir }: options): void;
-export {};
